a1e7b70f48b5ee83605ac06851944da55e3d4d81
HRITHIK-SANKAR-R <hrithiksrr@gmail.com>
2026-07-19 18:30:18 +0530

    chore: remove unused resend email idle worker

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -35,11 +35,10 @@ func main() {
 
 	// Start Idle & Archiver Worker
 	worker.InitArchiver()
-	ticker := time.NewTicker(1 * time.Minute)
-	defer ticker.Stop()
 	go func() {
-		for range ticker.C {
-			worker.CheckIdleConversations(queries)
+		ticker := time.NewTicker(60 * time.Second)
+		for {
+			<-ticker.C
 			worker.ArchiveOldConversations(queries)
 		}
 	}()
@@ -157,10 +156,9 @@ func main() {
 	// CORS Middleware
 	corsMiddleware := func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+			w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins for dev
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusOK)
 				return
